dd77b670a9ab0df72f5d6caeea861c4c0c00336c
Pranav Sharma <88233021+Cyberpunkpranav@users.noreply.github.com>
2024-11-19 08:22:30 +0530

    123

--- pure_stitch/app/arrivals/[type_name]/client.jsx
+++ pure_stitch/app/arrivals/[type_name]/client.jsx
@@ -7,9 +7,9 @@ const Arrivals = ({data,type_name}) => {
   console.log(data);
   
   return (  
-    <div className={styles['new-arrival']}>
+    <div className={`${styles['new-arrival']} bg-gradient${Math.random()*3}`}>
             <div className={styles['arrivals']}>
-              <Image className={styles['arrival-images']} unoptimized={true} height={100} width={100} src={`${process.env.ARRIVAL_POST_MEDIA}?width=${data[0].original_width}&height=${data[0].original_height}&image=${data[0].file}&category=${`t-shirts`}&quality=${100}&format=${data[0].format}`}/>
+              <Image className={styles['arrival-images']} unoptimized={true} height={100} width={100} src={`${process.env.ARRIVAL_POST_MEDIA}?width=${data[0].original_width/10}&height=${data[0].original_height/10}&image=${data[0].file}&category=${`t-shirts`}&quality=${100}&format=${data[0].format}`}/>
             </div>
     </div>
   )
